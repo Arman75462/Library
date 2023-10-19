2e6afa96d236df565c1c549f7a07df9a7e0f869d
Arman <armangulistani@gmail.com>
2023-10-19 13:00:49 -0400

    Implemented support for adding multiple books to the library
    This change allows users to add multiple books to the library at once.

--- script.js
+++ script.js
@@ -13,28 +13,18 @@ const bookAuthorInput = document.querySelector(".form__author");
 const bookPagesInput = document.querySelector(".form__pages");
 const bookReadInput = document.querySelector(".form__read");
 
-/* Book-info selectors */
-let bookInfo = document.querySelector(".book-info");
-let bookInfoTitle = document.querySelector(".book-info__title");
-let bookInfoAuthor = document.querySelector(".book-info__author");
-let bookInfoPages = document.querySelector(".book-info__pages");
-let bookInfoRead = document.querySelector(".book-info__read");
-let bookInfoRemove = document.querySelector(".book-info__remove");
-
-// Where your books are
+// Create an array to store your library of books
 const myLibrary = [];
-
-// Displays the books inside myLibrary on the console
-function addBookToLibrary() {
-  myLibrary.forEach(function (book) {});
-}
 
 // Function to hide the form and reveal the main content
 function hideForm() {
   form.classList.add("hidden");
   header.classList.remove("blur");
   main.classList.remove("blur");
-  navButton.classList.remove("blur");
+
+  // Transitions for smooth effect when hiding form
+  header.style.transition = "all 500ms";
+  main.style.transition = "all 500ms";
 }
 
 // Function to show the form and blur other content
@@ -42,20 +32,80 @@ function showForm() {
   form.classList.remove("hidden");
   header.classList.add("blur");
   main.classList.add("blur");
+
+  // Transitions for smooth effect when showing form
+  header.style.transition = "all 500ms";
+  main.style.transition = "all 500ms";
 }
 
-// Event listener to make form appear when clicking nav__button
+// Event listener to toggle the form visibility when clicking the "+ Add Book" button
 navButton.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     showForm(); // Show the form
-    header.style.transition = "all 500ms";
-    main.style.transition = "all 500ms";
   } else {
     hideForm(); // Hide the form
-    header.style.transition = "all 500ms";
-    main.style.transition = "all 500ms";
   }
 });
+
+// Function to display all books in the library
+function displayBooks() {
+  main.innerHTML = ""; // Clear the main element
+
+  myLibrary.forEach(function (book, index) {
+    // Create a new book-info element for each book
+    const bookInfoElement = document.createElement("section");
+    bookInfoElement.classList.add("book-info");
+
+    // Create elements to display book information
+    const titleElement = document.createElement("h2");
+    titleElement.classList.add("book-info__title");
+    titleElement.textContent = book.title;
+
+    const authorElement = document.createElement("p");
+    authorElement.classList.add("book-info__author");
+    authorElement.textContent = book.author;
+
+    const pagesElement = document.createElement("p");
+    pagesElement.classList.add("book-info__pages");
+    pagesElement.textContent = `${book.pages} pages`;
+
+    const readElement = document.createElement("span");
+    readElement.classList.add("book-info__read");
+    readElement.textContent = book.read ? "Read" : "Not read";
+
+    if (book.read) {
+      readElement.style.border = "2px solid green";
+      readElement.style.backgroundColor = "rgba(0, 174, 0, 0.524)";
+    } else {
+      readElement.style.border = "2px solid red";
+      readElement.style.backgroundColor = "rgba(255, 0, 0, 0.618)";
+    }
+
+    const removeElement = document.createElement("span");
+    removeElement.classList.add("book-info__remove");
+    removeElement.textContent = "Remove";
+
+    // Remove the book from the library and re-display the books
+    removeElement.addEventListener("click", function () {
+      // Remove the book from the library at the specific index
+      myLibrary.splice(index, 1);
+
+      // Re-display the books to reflect the removal
+      displayBooks();
+    });
+
+    // Append elements to the book-info element
+    bookInfoElement.appendChild(titleElement);
+    bookInfoElement.appendChild(authorElement);
+    bookInfoElement.appendChild(pagesElement);
+    bookInfoElement.appendChild(readElement);
+    bookInfoElement.appendChild(removeElement);
+
+    // Append the book-info element to the main element
+
+    main.appendChild(bookInfoElement);
+  });
+}
 
 submitBookButton.addEventListener("click", function () {
   // Create a new book object with user input
@@ -69,23 +119,8 @@ submitBookButton.addEventListener("click", function () {
   // Add the book to the library
   myLibrary.push(book);
 
-  // Update the book info section with user input
-  bookInfo.classList.remove("hidden");
-  bookInfoTitle.textContent = bookTitleInput.value;
-  bookInfoAuthor.textContent = bookAuthorInput.value;
-  bookInfoPages.textContent = `${bookPagesInput.value} pages`;
-
-  if (bookReadInput.checked === true) {
-    bookInfoRead.textContent = "Read";
-    bookInfoRead.style.border = "2px solid green";
-    bookInfoRead.style.backgroundColor = "rgba(0, 174, 0, 0.524)";
-  } else {
-    bookInfoRead.textContent = "Not read";
-    bookInfoRead.style.border = "2px solid red";
-    bookInfoRead.style.backgroundColor = "rgba(255, 0, 0, 0.618)";
-  }
-
-  addBookToLibrary();
+  // Display all books in the library
+  displayBooks();
 
   // Reset form input fields
   hideForm();
@@ -101,6 +136,8 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+
+  // A function to get book information
   this.info = function () {
     return `${title} by ${author}, ${pages} pages, ${
       read ? "has been read" : "hasn't been read yet"
